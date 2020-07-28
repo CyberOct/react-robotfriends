b7eda7d8dba1cf741075cdb89719c2ab83e76a89
Stefan Butura <stefan.butura@gmail.com>
2020-07-28 22:27:49 +0300

    Updates

---
+++ precache-manifest.3579efabad63538514fe60978ba938be.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f35916276688c050b1835dd0f6a66fbd",
+    "url": "/react-robotfriends/index.html"
+  },
+  {
+    "revision": "6a44b6a1467e6b0abb5c",
+    "url": "/react-robotfriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "7745142d445ea0f4bd60",
+    "url": "/react-robotfriends/static/css/main.0caaca48.chunk.css"
+  },
+  {
+    "revision": "6a44b6a1467e6b0abb5c",
+    "url": "/react-robotfriends/static/js/2.e327f7ed.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-robotfriends/static/js/2.e327f7ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7745142d445ea0f4bd60",
+    "url": "/react-robotfriends/static/js/main.483fa487.chunk.js"
+  },
+  {
+    "revision": "e6390225f4ce16746ee9",
+    "url": "/react-robotfriends/static/js/runtime-main.f89e73ec.js"
+  },
+  {
+    "revision": "82e09f7862a1e26fb7dfe1d0f8adc87f",
+    "url": "/react-robotfriends/static/media/SEGA.82e09f78.TTF"
+  }
+]);
